b80072069d1a36c3cca96a4b75b32cc394da5740
luishs <luishsw21@gmail.com>
2024-08-14 16:35:16 -0600

    add: prefix path

--- quasar.config.js
+++ quasar.config.js
@@ -51,7 +51,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: 'https://afcoma.github.io/public-web-afcoma/',
+      publicPath: '/login',
       // analyze: true,
       // env: {},
       // rawDefine: {}
