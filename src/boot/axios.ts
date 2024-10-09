62a85ddb89f99382c86da429726f1afe05c87891
luishs <luishsw21@gmail.com>
2024-10-09 00:47:07 -0600

    fix: modal renew sesion y taable order

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { Dialog } from 'quasar';
+let isDialogShown = false;
 
 const api = axios.create({
   baseURL: 'https://apolo.afcoma.com.mx/v1/',
@@ -19,9 +21,52 @@ api.interceptors.request.use((config) => {
 
 api.interceptors.response.use(
   (response) => response,
-  (error) => {
+  async (error) => {
     if (error.response && error.response.status === 401) {
-      localStorage.setItem('isLogin', 'true');
+      if (!isDialogShown) {
+        isDialogShown = true;
+        Dialog.create({
+          title: 'Sesión Expirada',
+          message: 'Tu sesión ha expirado. ¿Quieres continuar trabajando?.',
+          persistent: true,
+          style: {
+            width: '630px',
+            maxWidth: '80vw',
+            height: '180px',
+            borderRadius: '24px',
+          },
+          ok: {
+            label: 'Continuar',
+            color: 'secondary',
+            rounded: true,
+            style: 'text-transform: none;',
+          },
+          cancel: {
+            label: 'Cerrar Sesión',
+            color: 'primary',
+            rounded: true,
+            style: 'text-transform: none;',
+          },
+        })
+          .onOk(async () => {
+            try {
+              const { headers } = await api.post('/login/renewtoken', {
+                _id: localStorage.getItem('identity'),
+              });
+              localStorage.removeItem('token');
+              localStorage.setItem('token', headers.token);
+              isDialogShown = false;
+              window.location.href = '/dashboard';
+            } catch (renewError) {
+              console.error('Error al renovar el token', renewError);
+            }
+          })
+          .onCancel(() => {
+            localStorage.clear();
+            window.location.href = '/';
+          });
+      }
+      return Promise.reject(error);
     }
     return Promise.reject(error);
   }
